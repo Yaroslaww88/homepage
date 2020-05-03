f632cd710fc312f3773830e5f5a9ae84c0323748
Yaroslaww88 <yaroslaww1@gmail.com>
2020-05-03 18:41:26 +0300

    Add simple form

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
 $( document ).ready(function() {
-  console.log("Hello world")
+  /**
+   * Modal 
+   */
+  var button = document.getElementById("question-button");
+  var modal = document.getElementById("send-email-modal");
+  var btnCloseModal = document.getElementById("send-email-modal-close");
+
+  /**
+   * Toggle Modal window
+   */  
+  button.onclick = function() {
+    modal.style.display = "block";
+  };
+
+  btnCloseModal.onclick = function() {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+  /**
+   * Input && submit
+   */
+  var inputEmail = $( "input[name='email']" );
+  var inputQuestion = $( "textarea[name='message']" );
+  var btnSubmit = document.getElementById("send-button");
+
+
+  /**
+   * Submit form button
+   */
+  btnSubmit.onclick = function() {
+    // Here should be an ajax call, of course, but we dont have backend
+    console.log("Form submitted with values: ", inputEmail.val(), inputQuestion.val());
+  }
 });
